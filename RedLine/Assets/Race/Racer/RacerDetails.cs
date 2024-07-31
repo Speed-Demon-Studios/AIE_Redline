a5b02e4adc6d802312e92232d25a2fc48735976d
Emmanuel Williams <emmanuel.letton@gmail.com>
2024-07-31 13:08:00 +0930

    Updated some movement settings

--- RedLine/Assets/Race/Racer/RacerDetails.cs
+++ RedLine/Assets/Race/Racer/RacerDetails.cs
@@ -7,16 +7,16 @@ public class RacerDetails : MonoBehaviour
     public int currentLap = 0;
     public int placement = 0;
     public int currentCheckpoint = 0;
-    private CheckpointHandler cHandler;
 
-    private void Awake()
-    {
-    }
+    private CheckpointHandler m_cHandler;
 
+    /// <summary>
+    /// Calculates the distance to the next checkpoint
+    /// </summary>
     public float NextCheckpointDistance()
     {
-        cHandler = GameManager.gManager.checkpointParent.GetComponent<CheckpointHandler>();
-        var nextCheckpoint = cHandler.GetCheckpoint(cHandler.GetNextIndex(currentCheckpoint));
+        m_cHandler = GameManager.gManager.checkpointParent.GetComponent<CheckpointHandler>();
+        var nextCheckpoint = m_cHandler.GetCheckpoint(m_cHandler.GetNextIndex(currentCheckpoint));
         return Vector3.Distance(transform.position, nextCheckpoint.transform.position);
     }
 }
