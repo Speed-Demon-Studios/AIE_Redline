41c7fdf7c5cbb2840fed4b1142f01aa2be053c5c
Emmanuel Williams <emmanuel.letton@gmail.com>
2024-08-07 11:34:37 +0930

    Scriptable Object Changes

--- RedLine/Assets/Ships/Scipts/ShipVariants/PlayerShipVariant.cs
+++ RedLine/Assets/Ships/Scipts/ShipVariants/PlayerShipVariant.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ShipVariant", menuName = "Ship Variants/Ship Variant", order = 1)]
+[CreateAssetMenu(fileName = "ShipVariant", menuName = "Ship Variants/Player Ship Variant", order = 1)]
 public class ShipVariant : ScriptableObject
 {
     public string VariantName;
@@ -19,5 +19,8 @@ public class ShipVariant : ScriptableObject
     [Header("Turning Variables")]
     public float TurnSpeed;
     public AnimationCurve TurnSpeedCurve;
+
+    [Header("Ai Movement Variables")]
+    public AnimationCurve neededSpeedCurve;
     
 }
