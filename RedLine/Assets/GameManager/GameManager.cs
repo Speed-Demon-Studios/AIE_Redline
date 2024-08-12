74f59b1f74265abdabd1a687211eaeba69984831
Emmanuel Williams <emmanuel.letton@gmail.com>
2024-08-12 11:20:58 +0930

    changes to scriptable objects
    implemented a countdown for the races
    updated values

--- RedLine/Assets/GameManager/GameManager.cs
+++ RedLine/Assets/GameManager/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
     public IList<GameObject> playerObjects = new List<GameObject>();
     public IList<GameObject> racerObjects = new List<GameObject>();
     public bool raceStarted = false;
+    public int countdownIndex = 5;
 
     public static GameManager gManager { get; private set; }
 
