d87bca269def5191cdda26e3b1bbd6001f6544d5
Jaxon - AIE <126209068+Sk1ll1ssue@users.noreply.github.com>
2024-08-19 13:44:11 +0930

    Redline Mechanic
    starting to work

--- RedLine/Assets/Ships/Scipts/Redline/RedlineColliderSpawner.cs
+++ RedLine/Assets/Ships/Scipts/Redline/RedlineColliderSpawner.cs
@@ -1,35 +1,80 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RedlineColliderSpawner : MonoBehaviour
 {
-    public TrailRenderer trailLine;
-    MeshCollider m_lineCollider = new();
-    public bool start;
+    List<GameObject> m_lineColliders = new();
+    public List<GameObject> m_shipsInColliders = new();
+    public List<GameObject> ships = new();
+    private int childIndex;
+
+    public GameObject colliderPrefab;
+    public Transform spawnPoint;
+    public GameObject colliderParent;
+
     // Start is called before the first frame update
     void Start()
     {
-        m_lineCollider = GetComponent<MeshCollider>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        GenerateMeshCollider();
+        SpawnCollider();
+        CheckList();
+        CheckShipsInLine();
     }
 
-    public void GenerateMeshCollider()
+    public void CheckShipsInLine()
     {
-        if(m_lineCollider == null)
-            m_lineCollider = gameObject.AddComponent<MeshCollider>();
+        for (int i = 0; i < m_shipsInColliders.Count; i++)
+        {
+            if (ships.Contains(m_shipsInColliders[i]))
+                return;
+            else
+            {
+                m_shipsInColliders.Remove(m_shipsInColliders[i]);
+            }
+        }
+        for (int i = 0; i < ships.Count; i++)
+        {
+            if (m_shipsInColliders.Contains(ships[i]))
+                return;
+            else
+            {
+                m_shipsInColliders.Add(ships[i]);
+            }
+        }
 
-        Mesh mesh = new();
+        for(int i = 0; i < m_shipsInColliders.Count; i++)
+        {
+            ShipsControls controls = m_shipsInColliders[i].GetComponent<ShipsControls>();
 
-        trailLine.BakeMesh(mesh, true);
-
-        m_lineCollider.sharedMesh = mesh;
+            controls.AddToBoost();
+        }
     }
 
+    private void CheckList()
+    {
+        m_lineColliders[childIndex].gameObject.transform.position = spawnPoint.transform.position;
+        childIndex += 1;
+        if (childIndex > m_lineColliders.Count - 1)
+            childIndex = 0;
+    }
+
+    public void SpawnCollider()
+    {
+        if (m_lineColliders.Count < 50)
+        {
+            GameObject a = Instantiate(colliderPrefab, spawnPoint.position, Quaternion.Euler(Vector3.zero), colliderParent.transform);
+
+            ColliderTrigger b = a.GetComponent<ColliderTrigger>();
+            b.spawner = this;
+
+            m_lineColliders.Add(a);
+        }
+    }
 }
