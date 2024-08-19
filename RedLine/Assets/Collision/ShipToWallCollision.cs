744493480284df42eacb045111b8ed3d7b5c2a75
Emmanuel Williams <emmanuel.letton@gmail.com>
2024-08-19 11:37:58 +0930

    started implementing the ship-to-wall collision

---
+++ RedLine/Assets/Collision/ShipToWallCollision.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipToWallCollision : MonoBehaviour
+{
+    [SerializeField] private ShipsControls sControlScript;
+    [SerializeField] private float knockbackPercentage = 1.3f;
+    [SerializeField] private float changedTopSpeed;
+    [SerializeField] private float changedAcceleration;
+    private ShipVariant shipVariant;
+    private float defaultTopSpeed;
+    private float defaultAcceleration;
+    bool intoWall = false;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag.ToLower() == "walls")
+        {
+            intoWall = true;
+        }
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.transform.tag.ToLower() == "walls" && intoWall == true)
+        {
+            shipVariant.MaxSpeed = changedTopSpeed;
+            shipVariant.MaxAcceleration = changedAcceleration;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        shipVariant.MaxSpeed = defaultTopSpeed;
+        shipVariant.MaxAcceleration = defaultAcceleration;
+        if (other.transform.tag.ToLower() == "walls" && intoWall == true)
+        {
+            intoWall = false;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipVariant = sControlScript.Variant;
+        defaultTopSpeed = shipVariant.MaxSpeed;
+        defaultAcceleration = shipVariant.MaxAcceleration;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        
+    }
+}
