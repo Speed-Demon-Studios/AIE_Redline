c8666b1b47e2b84580df179839632b9aad4de474
Emmanuel Williams <emmanuel.letton@gmail.com>
2024-09-09 13:14:38 +0930

    End of race panel and name randomization fixes
    Fixed issues with the end of race panel that was causing it to show when not all players had crossed the finish line.

--- RedLine/Assets/Race/FinishRace.cs
+++ RedLine/Assets/Race/FinishRace.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using System.Net;
 
 public class FinishRace : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class FinishRace : MonoBehaviour
     [SerializeField] private GameObject placementWindow;
     private TextMeshProUGUI[] tempSortingTextList;
     private bool m_allRacersFinished = false;
+    private bool m_alreadyShowingPlacements = false;
+    private bool m_allRacersCrosedLine = false;
+    private bool m_checkingRacersFinished = false;
+    private bool m_racersFinishedChecked = false;
     private bool readyToSetSelected = false;
     private bool readyToDisplay = false;
     private bool timingsListsUpdated = false;
@@ -21,6 +26,7 @@ public class FinishRace : MonoBehaviour
     private void Awake()
     {
         GameManager.gManager.raceFinisher = this;
+        m_alreadyShowingPlacements = false;
     }
 
     public void DebugForFunction()
@@ -29,137 +35,130 @@ public class FinishRace : MonoBehaviour
         Debug.Log("button clicked.");
     }
 
+    private void Update()
+    {
+        CheckAllRacersFinished();
+
+        
+    }
+
     /// <summary>
     /// Enables and updates information for the placement window, and activates UI input for player 1.
     /// </summary>
     public void ShowFinalPlacements()
     {
-        foreach (GameObject racer in GameManager.gManager.racerObjects)
+        if (m_allRacersFinished == true && m_allRacersCrosedLine == true)
         {
-            if (GameManager.gManager.players.Contains(racer) == false)
+            m_alreadyShowingPlacements = true;
+
+            foreach (GameObject racer in GameManager.gManager.racerObjects)
             {
-                RacerDetails rDeets = racer.GetComponent<RacerDetails>();
-
-                rDeets.finishedRacing = true;
-            }
-        }
-        // Enable the race placement display window and its child objects.
-        if (placementWindow.activeSelf == false)
-        {
-            placementWindow.SetActive(true);
-        }
-
-        ActionMappingControl aMC = GameManager.gManager.players[0].GetComponent<ActionMappingControl>(); // Get a reference to player ones ActionMappingControl script.
-        aMC.mES.firstSelectedGameObject = mainButton; // Set player ones MultiplayerEventSystem's firstSelectedGameObject to the mainButton object.
-        aMC.mES.SetSelectedGameObject(mainButton); // Set player ones MultiplayerEventSystem's selectedGameObject to the mainButton object.
-
-        // Iterate through all of the racer objects.
-        for (int i = 0; i < GameManager.gManager.racerObjects.Count; i++)
-        {
-            placementTexts[i].SetActive(true); // Activate a text object in the placement window for each racer.
-            TextMeshProUGUI placementText = placementTexts[i].GetComponent<TextMeshProUGUI>(); // Get a reference to the text object.
-            //foreach (GameObject textOBJ in placementTexts)
-            //{
-            //
-            //}
-
-            // Iterate through all of the racer objects again, this time to update the text objects with each racer's respective name and placement.
-            foreach (GameObject racerOBJ in GameManager.gManager.racerObjects)
-            {
-
-                RacerDetails racerDeets = racerOBJ.GetComponent<RacerDetails>();
-
-                if ((i + 1) == racerDeets.placement)
+                if (GameManager.gManager.players.Contains(racer) == false)
                 {
-                    //float bestLapTimeSEC = 0f;
-                    //float bestLapTimeMIN = 0f;
-                    //if (racerDeets.lapTimesMINUTES.Count > 0 && racerDeets.lapTimesSECONDS.Count > 0)
-                    //{
-                    //    bestLapTimeSEC = racerDeets.lapTimesSECONDS[0];
-                    //    bestLapTimeMIN = racerDeets.lapTimesMINUTES[0];
-                    //}
-                    //for (int a = 0; a < racerDeets.lapTimesSECONDS.Count; a++)
-                    //{
-                    //    foreach (float lapSECONDS in racerDeets.lapTimesSECONDS)
-                    //    {
-                    //        if (lapSECONDS > racerDeets.lapTimesSECONDS[a])
-                    //        {
-                    //            bestLapTimeSEC = lapSECONDS;
-                    //        }
-                    //    }
-                    //}
-                    //for (int a = 0; a < racerDeets.lapTimesMINUTES.Count; a++)
-                    //{
-                    //    foreach (float lapMINUTES in racerDeets.lapTimesMINUTES)
-                    //    {
-                    //        if (lapMINUTES > racerDeets.lapTimesMINUTES[a])
-                    //        {
-                    //            bestLapTimeMIN = lapMINUTES;
-                    //        }
-                    //    }
-                    //
-                    //}
-                    readyToDisplay = true;
+                    RacerDetails rDeets = racer.GetComponent<RacerDetails>();
 
-                    //if (readyToDisplay == true)
-                    //{
-                    //    while (GameManager.gManager.timingsListUpdated == false)
-                    //    {
-                    //        if (GameManager.gManager.timingsListUpdated == true)
-                    //        {
-                    //            break;
-                    //        }
-                    //    }
-                    //}
+                    rDeets.finishedRacing = true;
+                }
+            }
+            // Enable the race placement display window and its child objects.
+            if (placementWindow.activeSelf == false)
+            {
+                placementWindow.SetActive(true);
+            }
 
-                    if (GameManager.gManager.players.Contains(racerOBJ))
+            ActionMappingControl aMC = GameManager.gManager.players[0].GetComponent<ActionMappingControl>(); // Get a reference to player ones ActionMappingControl script.
+            aMC.mES.firstSelectedGameObject = mainButton; // Set player ones MultiplayerEventSystem's firstSelectedGameObject to the mainButton object.
+            aMC.mES.SetSelectedGameObject(mainButton); // Set player ones MultiplayerEventSystem's selectedGameObject to the mainButton object.
+
+            // Iterate through all of the racer objects.
+            for (int i = 0; i < GameManager.gManager.racerObjects.Count; i++)
+            {
+                placementTexts[i].SetActive(true); // Activate a text object in the placement window for each racer.
+                TextMeshProUGUI placementText = placementTexts[i].GetComponent<TextMeshProUGUI>(); // Get a reference to the text object.
+
+                // Iterate through all of the racer objects again, this time to update the text objects with each racer's respective name and placement.
+                foreach (GameObject racerOBJ in GameManager.gManager.racerObjects)
+                {
+
+                    RacerDetails racerDeets = racerOBJ.GetComponent<RacerDetails>();
+
+                    if ((i + 1) == racerDeets.placement)
                     {
-                        if (racerDeets.finishedRacing == true)
+                        readyToDisplay = true;
+
+                        if (GameManager.gManager.players.Contains(racerOBJ))
                         {
-                            placementText.text = "(" + (racerDeets.placement) + ") " + racerDeets.RacerName + "    ||   " + racerDeets.totalRaceTimeMinutes + ":" + racerDeets.totalRaceTimeSeconds;
-                        }
-                    }
-                    else
-                    {
-                        if (racerDeets.crossedFinishLine == false)
-                        {
-                            placementText.text = racerDeets.RacerName + "   ||   DNF";
+                            if (racerDeets.finishedRacing == true)
+                            {
+                                placementText.text = "(" + (racerDeets.placement) + ") " + racerDeets.RacerName + "    ||   " + racerDeets.totalRaceTimeMinutes + ":" + racerDeets.totalRaceTimeSeconds;
+                            }
                         }
                         else
                         {
-                            placementText.text = "(" + (racerDeets.placement) + ") " + racerDeets.RacerName + "    ||   " + racerDeets.totalRaceTimeMinutes + ":" + racerDeets.totalRaceTimeSeconds;
+                            if (racerDeets.crossedFinishLine == false)
+                            {
+                                placementText.text = racerDeets.RacerName + "   ||   DNF";
+                            }
+                            else
+                            {
+                                placementText.text = "(" + (racerDeets.placement) + ") " + racerDeets.RacerName + "    ||   " + racerDeets.totalRaceTimeMinutes + ":" + racerDeets.totalRaceTimeSeconds;
+                            }
                         }
                     }
                 }
             }
-
         }
     }
 
 
     public void CheckAllRacersFinished()
     {
+        m_checkingRacersFinished = true;
+        m_racersFinishedChecked = false;
+
+        m_allRacersFinished = true;
+        m_allRacersCrosedLine = true;
+
         // Iterate through all of the PLAYER objects.
         foreach (GameObject racerOBJ in GameManager.gManager.players)
         {
             RacerDetails rDeets = racerOBJ.GetComponent<RacerDetails>();
-
-            if (rDeets.finishedRacing == true)
+            if (rDeets.crossedFinishLine == false)
             {
-                m_allRacersFinished = true;
+                m_allRacersCrosedLine = false;
             }
 
             if (rDeets.finishedRacing == false)
             {
                 m_allRacersFinished = false;
+                break;
             }
         }
 
-        if (m_allRacersFinished == true)
+        for (int i = 0; i < GameManager.gManager.players.Count; i++)
         {
-            Debug.Log("Race has been completed! All racers have crossed the finish line!");
-            GameManager.gManager.raceFinished = true;
+            m_allRacersFinished = true;
+            RacerDetails rDeets = GameManager.gManager.players[i].GetComponent<RacerDetails>();
+
+            if (rDeets.crossedFinishLine == false)
+            {
+                m_allRacersCrosedLine = false;
+            }
+
+            if (rDeets.finishedRacing == false)
+            {
+                m_allRacersFinished = false;
+                break;
+            }
+        }
+
+        m_racersFinishedChecked = true;
+        m_checkingRacersFinished = false;
+
+        if (m_allRacersFinished == true && m_alreadyShowingPlacements == false && m_allRacersCrosedLine == true)
+        {
             ShowFinalPlacements();
         }
+        return;
     }
 }
