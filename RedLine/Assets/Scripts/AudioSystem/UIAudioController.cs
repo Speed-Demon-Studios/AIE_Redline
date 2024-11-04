470db345da026be1c779adc89088848e207783e2
Emmanuel Williams <emmanuel.letton@gmail.com>
2024-11-04 16:39:00 +1030

    Updated the menu audio so the confirmation sound has small pitch variations

--- RedLine/Assets/Scripts/AudioSystem/UIAudioController.cs
+++ RedLine/Assets/Scripts/AudioSystem/UIAudioController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UIAudioController : MonoBehaviour
@@ -9,22 +10,61 @@ public class UIAudioController : MonoBehaviour
     public FMODUnity.StudioEventEmitter pauseEmitter;
     public FMODUnity.StudioEventEmitter cancelEmitter;
     public List<FMODUnity.EventReference> menuAudio = new List<FMODUnity.EventReference>();
+    public float[] pitchVariations;
+    public float selectedVariation;
+    public int selectedVariationIndex;
+    public float pitchOUT;
+
+    private void Start()
+    {
+        pauseEmitter.EventReference = menuAudio[0];
+        joinEmitter.EventReference = menuAudio[1];
+        confirmEmitter.EventReference = menuAudio[2];
+    }
 
     public void GamePauseSound()
     {
-        pauseEmitter.EventReference = menuAudio[0];
         pauseEmitter.Play();
     }
 
     public void PlayerJoinSound()
     {
-        joinEmitter.EventReference = menuAudio[1];
         joinEmitter.Play();
     }
 
     public void MenuConfirmSound()
     {
-        confirmEmitter.EventReference = menuAudio[2];
+        confirmEmitter.EventInstance.getPitch(out pitchOUT);
+        float pitchSelected = 0.0f;
+        pitchSelected = Random.Range(0.0f, 25.0f);
+
+        if (pitchSelected <= 5)
+        {
+            selectedVariation = pitchVariations[0];
+        }
+        else if (pitchSelected > 5 && pitchSelected <= 10)
+        {
+            selectedVariation = pitchVariations[1];
+        }
+        else if (pitchSelected > 10 && pitchSelected <= 15)
+        {
+            selectedVariation = pitchVariations[2];
+        }
+        else if (pitchSelected > 15 && pitchSelected <= 20)
+        {
+            selectedVariation = pitchVariations[3];
+        }
+        else if (pitchSelected > 20 && pitchSelected <= 25)
+        {
+            selectedVariation = pitchVariations[4];
+        }
+        confirmEmitter.EventInstance.getPitch(out pitchOUT);
+        confirmEmitter.EventInstance.setPitch(selectedVariation);
         confirmEmitter.Play();
+    }
+
+    private void Update()
+    {
+        confirmEmitter.EventInstance.setPitch(selectedVariation);
     }
 }
