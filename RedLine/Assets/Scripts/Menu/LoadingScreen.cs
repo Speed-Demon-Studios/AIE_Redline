add5b871e0a1dc2e8c769eb447eeb6d736c0bcf0
Jaxon - AIE <126209068+Sk1ll1ssue@users.noreply.github.com>
2024-11-06 14:15:15 +1030

    unReady
    you can now unready when you select a ship

--- RedLine/Assets/Scripts/Menu/LoadingScreen.cs
+++ RedLine/Assets/Scripts/Menu/LoadingScreen.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingScreen : MonoBehaviour
 {
+    public Image loadingBar;
+    float m_targetLoading;
+    float m_currentLoading;
+    string m_sceneID = "SceneID";
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_currentLoading = 0;
+        m_targetLoading = 0;
+        int Id = PlayerPrefs.GetInt(m_sceneID);
+        StartCoroutine(LoadingScene(Id));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadingScene(int sceneID)
     {
-        
+        AsyncOperation operation =  SceneManager.LoadSceneAsync(sceneID);
+
+        while (!operation.isDone)
+        {
+            m_targetLoading = operation.progress / 0.9f;
+            m_currentLoading = Mathf.Lerp(m_currentLoading, m_targetLoading, 0.03f);
+            loadingBar.fillAmount += m_currentLoading;
+            yield return null;
+        }
+
     }
 }
