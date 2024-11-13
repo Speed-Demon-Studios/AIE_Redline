c6fdd49d96e2371548891e957aeec8b239be73fd
DanielDCruzAIE <128771290+DanielDCruzAIE@users.noreply.github.com>
2024-11-13 13:52:32 +1030

    Finished Class select menu

--- RedLine/Assets/Scripts/Menu/ButtonSelectManager.cs
+++ RedLine/Assets/Scripts/Menu/ButtonSelectManager.cs
@@ -1,6 +1,8 @@
 using Pixelplacement;
 using System.Collections;
 using System.Collections.Generic;
+//using System.Drawing;
+//using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,20 +15,38 @@ namespace DifficultyButtonSwitch
         [SerializeField]
         private float m_barChangeDuration, m_barLength;
         [SerializeField]
-        private Transform m_speedBar, m_competitionSkillBar;
+        private Transform m_speedBar, m_speedBarRed1, m_speedBarRed2, m_competitionSkillBar, m_competitionSkillBarRed1, m_competitionSkillBarRed2;
         [SerializeField]
         Animator m_debutAnimator, m_proAnimator, m_eliteAnimator;
+
+        [SerializeField]
+        private Material m_cursorMat;
+
+        [SerializeField]
+        private AnimationCurve m_cursorPulseCurve;
+
+        private void OnEnable()
+        {
+            Tween.Color(m_cursorMat, Color.red, 1, 0, m_cursorPulseCurve, Tween.LoopType.Loop);
+        }
 
         public void SpeedBarFill(float fillAmount)
         {
             Vector3 barPos = new Vector3(m_barLength - (m_barLength * fillAmount), 0, 0);
-            Tween.LocalPosition(m_speedBar, barPos, m_barChangeDuration, 0, m_barChangeTween);
+            Tween.LocalPosition(m_speedBar, barPos, m_barChangeDuration, 0.05f, m_barChangeTween);
+            Tween.LocalPosition(m_speedBarRed1, barPos, m_barChangeDuration, 0, m_barChangeTween);
+            Tween.LocalPosition(m_speedBarRed2, barPos, m_barChangeDuration, 0.1f, m_barChangeTween);
         }
+    
+
+    
 
         public void CompetitionSkillBarFill(float fillAmount)
         {
             Vector3 barPos = new Vector3(m_barLength - (m_barLength * fillAmount), 0, 0);
-            Tween.LocalPosition(m_competitionSkillBar, barPos, m_barChangeDuration, 0, m_barChangeTween);
+            Tween.LocalPosition(m_competitionSkillBar, barPos, m_barChangeDuration, 0.05f, m_barChangeTween);
+            Tween.LocalPosition(m_competitionSkillBarRed1, barPos, m_barChangeDuration, 0, m_barChangeTween);
+            Tween.LocalPosition(m_competitionSkillBarRed2, barPos, m_barChangeDuration, 0.1f, m_barChangeTween);
         }
 
         public void SpeedClassInfoChange(int speed)
