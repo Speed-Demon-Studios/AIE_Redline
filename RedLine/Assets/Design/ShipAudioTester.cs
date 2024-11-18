c226ac46a4b396d4814461f708d96377b41fe322
DanielDCruzAIE <128771290+DanielDCruzAIE@users.noreply.github.com>
2024-11-18 11:34:53 +1030

    Created ship audio tester

--- RedLine/Assets/Design/ShipAudioTester.cs
+++ RedLine/Assets/Design/ShipAudioTester.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ShipAudioTester : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    [Range(0f, 1f)]
+    float m_shipSpeed;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    AudioSource m_mainEngine, m_hum, m_wind;
+
+    [SerializeField]
+    AnimationCurve m_mainEnginePitch, m_humPitch, m_windPitch, m_mainEngineVolume, m_humVolume, m_windVolume;
+
+    private void FixedUpdate()
     {
-        
+        m_mainEngine.pitch = m_mainEnginePitch.Evaluate(m_shipSpeed);
+        m_hum.pitch = m_humPitch.Evaluate(m_shipSpeed);
+        m_wind.pitch = m_windPitch.Evaluate(m_shipSpeed);
+
+        m_mainEngine.volume = m_mainEngineVolume.Evaluate(m_shipSpeed);
+        m_hum.volume = m_humVolume.Evaluate(m_shipSpeed);
+        m_wind.volume = m_windVolume.Evaluate(m_shipSpeed);
     }
 }
